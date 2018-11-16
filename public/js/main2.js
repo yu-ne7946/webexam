d44308b2935ea2f56e53e98a402dfae2dfcd923b
yu-ne7946 <carpdm7946@naver.com>
2018-11-16 18:49:59 +0900

    1811161849

--- public/js/main2.js
+++ public/js/main2.js
@@ -1,3 +1,72 @@
+
+
+$(".pt_box").hover(function(){
+    $(this).find(".pt_black").stop().animate({"top":"0%", "height":"100%"}, 400);
+  
+    $(this).find(".pt_border").stop().animate({"height":"95%","margin":"2% auto 1%"}, 400);
+    $(this).find(".cell_title").css("bottom","-15px");
+    $(this).find(".pt_cell h2").css("display","block");
+    $(this).find(".pt_cell p").css("display","block");
+    $(this).find(".detail").css("display","block");
+
+}, function(){
+    $(this).find(".pt_black").stop().animate({"top":"40%", "height":"20%"}, 400);
+    $(this).find(".pt_border").stop().animate({"width":"95%","height":"60%","margin":"3% auto 2%"}, 400);
+    $(this).find(".cell_title").css("bottom","-12px");
+    $(this).find(".pt_cell h2").css("display","none");
+    $(this).find(".pt_cell p").css("display","none");
+    $(this).find(".detail").css("display","none");
+});
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBjStabE0pYryBrCwywPl-nlwxiRguU0lM",
+    authDomain: "yune-webexam.firebaseapp.com",
+    databaseURL: "https://yune-webexam.firebaseio.com",
+    projectId: "yune-webexam",
+    storageBucket: "yune-webexam.appspot.com",
+    messagingSenderId: "818997460867"
+  };
+  firebase.initializeApp(config);
+
+  
+  /**전역변수 */
+  var auth=firebase.auth();
+  var db= firebase.database();
+  var googleAuth = new firebase.auth.GoogleAuthProvider();
+  var ref;
+  var user;
+  var key = '';
+
+
+    /**구글로그인 */
+    $(".head_but .login").on("click", function(){
+        // auth.signInWithPopup(googleAuth);
+        auth.signInWithRedirect(googleAuth);
+    });
+
+
+/**로그아웃 */
+$(".out .logout").click(function(){
+    auth.signOut();
+})
+
+auth.onAuthStateChanged(function(){
+    if(result){
+        user = result;
+        init();
+    }
+    else{
+        $(".head_but .login").show();
+        $(".lists").empty();
+    }
+});
+
+
+
+
 /* $(".pt_cell").hover(function () {
     $(".pt_black").css({
         "background-color": "rgba(0,0,0,0.6)",
@@ -56,21 +125,3 @@
 .pt_black{top:0; height:100%;}
 .pt_border{height:90%;}
 */
-
-$(".pt_box").hover(function(){
-    $(this).find(".pt_black").stop().animate({"top":"0%", "height":"100%"}, 400);
-  
-    $(this).find(".pt_border").stop().animate({"height":"95%","margin":"2% auto 1%"}, 400);
-    $(this).find(".cell_title").css("bottom","-15px");
-    $(this).find(".pt_cell h2").css("display","block");
-    $(this).find(".pt_cell p").css("display","block");
-    $(this).find(".detail").css("display","block");
-
-}, function(){
-    $(this).find(".pt_black").stop().animate({"top":"40%", "height":"20%"}, 400);
-    $(this).find(".pt_border").stop().animate({"width":"95%","height":"60%","margin":"3% auto 2%"}, 400);
-    $(this).find(".cell_title").css("bottom","-12px");
-    $(this).find(".pt_cell h2").css("display","none");
-    $(this).find(".pt_cell p").css("display","none");
-    $(this).find(".detail").css("display","none");
-});
