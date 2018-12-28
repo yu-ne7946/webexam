b783ad009f5dd98fe1ac6ba57ffc8ff42ef1eac6
yu-ne7946 <carpdm7946@naver.com>
2018-12-28 15:07:26 +0900

    181228

--- public/js/main2.js
+++ public/js/main2.js
@@ -1,5 +1,3 @@
-
-
 var isMobile = false; //initiate as false
 // device detection
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
@@ -467,7 +465,6 @@ function revData(obj) {
     $(".pt_cell h2").css("display", "none");
     $(".pt_cell p").css("display", "none");
     4(".detail").css("display", "none");
-
 }); */
 
 /*
